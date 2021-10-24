7a852291b99177766e126cf9d20599dc47d5888a
Daymao Silva <73561299+daymao@users.noreply.github.com>
2021-10-24 15:25:09 -0400

    Uploaded updated files

--- energyConservation.js
+++ energyConservation.js
@@ -7,17 +7,21 @@ function RenderEnergyConservationPage(){
     //Load the main div
     let mainDivE = document.getElementById("main");
 
-    //Creates a header element where the nav will go
+    //Creates a div where information about purpose of page will be stored
     let pagePurpose = document.createElement("div")
     pagePurpose.classList.add("pagePurposeDiv");
 
+    //Update the text and add the pagePurpose div to the mainDivE
+    pagePurpose.innerHTML += 'This page is dedicated to giving energy conservation tips to <br>';
+    pagePurpose.innerHTML += 'anyone who wants to reduce their energy usage to save money and <br>';
+    pagePurpose.innerHTML += 'aid in reducing global warming.';
+    mainDivE.appendChild(pagePurpose);
+
+    
     //let logoImage = document.createElement("img");
     //logoImage.classList.add("logo_image")
     //logoImage.src = "files/Header_no_background.png";
     //navBarHeader.appendChild(logoImage);
-
-    pagePurpose.innerHTML += 'This page is for energy conservation ';
-    mainDivE.appendChild(pagePurpose);
 }
 
 navbar()
