dae51bbf640b7b60a9c6c87e1ab05d26986214df
Erick Gonzalez <62159920+erickcgt@users.noreply.github.com>
2021-10-25 18:27:53 -0400

    updated heatmap.js
    made map bigger, added markers, and added heatmap weights.

--- heatmap.js
+++ heatmap.js
@@ -22,49 +22,59 @@ function LoadHeatmap(){
   mainDiv = document.getElementById('main')
   mapDiv = document.createElement('div')
   mapDiv.id = 'map'
-  mapDiv.style.height = "400px"
-  mapDiv.style.width = "550px"
+  mapDiv.style.height = "550px"
+  mapDiv.style.width = "750px"
   mapDiv.style.position = "relative"
   mapDiv.style.margin = "0 auto"
   mapDiv.style.top = "80px"
   map = new google.maps.Map(mapDiv, {
-  center: {lat: 28.1, lng: -82.9},
-  zoom: 6.1
+  center: {lat: 28.1, lng: -84.2},
+  zoom: 6.7
   });
-  //placeholder data
+  //PLACEHOLDER DATA TO BE REPLACED BY JSON FROM DATABASE
   json = {
     "counties":
     [
       {
-        "name": "Miami", 
-        "coordinates": [25.7617, -80.2]
+        "name": "Miami-Dade County", 
+        "coordinates": "25.55, -80.63",
+        "population": "2700794"
       },
       {
-        "name": "Gainesville", 
-        "coordinates": [29.65, -82.32]
+        "name": "Alachua County", 
+        "coordinates": "29.65, -82.32",
+        "population": "269043"
+      },
+      {
+        "name": "Orange County", 
+        "coordinates": "28.48, -81.25",
+        "population": "1145956"
       }
     ]
   }
+  //POPULATING MAP WINDOW
   heatMapData = []
   counties = new Map()
   for (i in json.counties){
+    //heatmap data
     countyObj = new County(json.counties[i])
+    currLocation = new google.maps.LatLng(countyObj.coordinates[0], countyObj.coordinates[1])
+    currWeight = countyObj.population/2700794
+    dataPoint = {location: currLocation, weight: currWeight}
+    heatMapData.push(dataPoint)
+    //marker data
+    new google.maps.Marker({
+      position: currLocation,
+      map,
+      title: countyObj.name,
+    });
+    //store in hashmap
     counties.set(countyObj.name, countyObj)
-    datapoint = new google.maps.LatLng(countyObj.coordinates[0], countyObj.coordinates[1])
-    heatMapData.push(datapoint)
   }
-  /*
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "Hello World!",
-  });
-  */
-  console.log(counties)
-  console.log(heatMapData)
   //heatmap
   var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: heatMapData
+    data: heatMapData,
+    radius: 60
   });
   heatmap.setMap(map);
   
@@ -73,22 +83,27 @@ function LoadHeatmap(){
   console.log("heatmap() called")
 }
 
-
 //County Class
 class County {
   //constructor
   constructor(json) {
     this.name = json.name;
-    this.coordinates = json.coordinates;
-    this.radius = calculateRadius(json.squareft);
+    let coordinatesArr = json.coordinates.split(", ")
+    this.coordinates = [parseFloat(coordinatesArr[0]), parseFloat(coordinatesArr[1])]
+    this.radius = calculateRadius(json.squareft)
+    this.population = parseInt(json.population)
     this.consumption = json.consumption;
     this.production = json.production;
     this.population = json.population;
     //to do: add other relevant data
   }
+  setMarker(marker){
+    this.marker = marker
+  }
 }
 //calculate heatmap node radius from square feet of county
 function calculateRadius(squareft){
   //to do: function body
+  squareft = parseInt(squareft)
   return 5
 }
