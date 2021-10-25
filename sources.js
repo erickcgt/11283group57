cec66aa199261ce6370d106cd5de0a663e9d7d78
annalinab <90287147+annalinab@users.noreply.github.com>
2021-10-24 21:57:12 -0400

    added to energy conservation page

--- sources.js
+++ sources.js
@@ -12,6 +12,8 @@ function RenderSourcesPage(){
     sources.classList.add("sources");
 
     //Update the text and add the pagePurpose div to the mainDivE
+    sources.innerHTML += 'References <br>';
+    sources.innerHTML += '<br>';
     sources.innerHTML += '1. https://www.nrdc.org/stories/easy-ways-save-energy-home?gclid=CjwKCAjw_L6LBhBbEiwA4c46uiVdU9MUhFpFBdRwUYFNcCEtlSbfdvy4hmRAJJBODPHJ9tfeGk8dqBoCx24QAvD_BwE <br>';
     sources.innerHTML += '<br>';
     sources.innerHTML += '2. https://www.nrdc.org/stories/easy-ways-save-energy-home?gclid=CjwKCAjw_L6LBhBbEiwA4c46uiVdU9MUhFpFBdRwUYFNcCEtlSbfdvy4hmRAJJBODPHJ9tfeGk8dqBoCx24QAvD_BwE <br>';
@@ -19,6 +21,12 @@ function RenderSourcesPage(){
     sources.innerHTML += '3. https://www.eia.gov/state/?sid=FL <br>';
     sources.innerHTML += '<br>';
     sources.innerHTML += '4. https://www.energysage.com/energy-efficiency/why-conserve-energy/cost-of-ee/ <br>';
+    sources.innerHTML += '<br>';
+    sources.innerHTML += '5. https://taraenergy.com/blog/your-guide-to-understanding-energy-conservation/ <br>';
+    sources.innerHTML += '<br>';
+    sources.innerHTML += '6. https://www.conserve-energy-future.com/whyconserveenergy.php/ <br>';
+    sources.innerHTML += '<br>';
+    sources.innerHTML += '7. https://www.bhhclv.com/7-reasons-to-conserve-energy// <br>';
     sources.innerHTML += '<br>';
     mainDivS.appendChild(sources);
 }
