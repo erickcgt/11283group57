5648b4facc389aa3334cefd20796be3b679e2343
Annalina Becker <90287147+annalinab@users.noreply.github.com>
2021-10-25 17:20:08 -0400

    changed energy conservation and sources

--- energyConservation.js
+++ energyConservation.js
@@ -12,25 +12,34 @@ function RenderEnergyConservationPage(){
     pagePurpose.classList.add("pagePurposeDiv");
 
     //Update the text and add the pagePurpose div to the mainDivE
-    pagePurpose.innerHTML += 'This page is dedicated to giving energy conservation tips to <br>';
-    pagePurpose.innerHTML += 'anyone who wants to reduce their energy usage to save money and <br>';
-    pagePurpose.innerHTML += 'aid in reducing global warming.';
+    pagePurpose.innerHTML += 'This page is dedicated to providing energy conservation tips to <br>';
+    pagePurpose.innerHTML += 'those who want to reduce their energy usage to save money as well as <br>';
+    pagePurpose.innerHTML += 'improve their ecological footprint.';
     mainDivE.appendChild(pagePurpose);
+
+    //Code to add image onto our page
+    let energyConDef = document.createElement("img");
+    energyConDef.classList.add("con_image")
+    energyConDef.src = "files/energyConDef.png";
+    mainDivE.appendChild(energyConDef);
 
     //Code to add image onto our page
     let conservationImage = document.createElement("img");
     conservationImage.classList.add("con_image")
     conservationImage.src = "files/ConservationTips_ClearerText.png";
-    mainDivE.appendChild(conservationImage);
+    mainDivE.appendChild(conservationImage);    
 
-    //Code where we can add tips for people to save energy
-    let conservationTipsDiv = document.createElement("div")
-    conservationTipsDiv.classList.add("consTipsDiv");
+    //Code to add image onto our page
+    let energyBenefits = document.createElement("img");
+    energyBenefits.classList.add("con_image")
+    energyBenefits.src = "files/energyConBenefits.png";
+    mainDivE.appendChild(energyBenefits);    
 
-    conservationTipsDiv.innerHTML += 'The U.S. Department of Energy estimates that the typical household can save 25% on utility bills<br>';
-    conservationTipsDiv.innerHTML += 'with energy efficiency measures, which amounts to over $2,200 annually. <br>';
-    conservationTipsDiv.innerHTML += "Aside from tips provided in the image above, here are a few more tips for you to save energy: <br>";
-    mainDivE.appendChild(conservationTipsDiv);
+    //Code to add image onto our page
+    let savingMoneyStatImage = document.createElement("img");
+    savingMoneyStatImage.classList.add("con_image")
+    savingMoneyStatImage.src = "files/saveMoneyStatistic.png";
+    mainDivE.appendChild(savingMoneyStatImage);
 }
 
 navbar()
